1b8af52d6649975b31b3349f82a353d9277c14f0
KstroO <Fama122010@hotmail.com>
2022-06-19 22:21:39 -0500

    Creation of "public" folder, organization of styles and client side js to this folder

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express')
 const {MongoClient, ObjectId} = require('mongodb')
 const cors = require('cors')
-// const {req} = require('express')
-// const {res} = require('http')
 const app = express()
 require('dotenv').config()
 
 PORT = 8000
 let db, 
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'sample_mflix',
-    collection
+dbConnectionStr = process.env.DB_STRING,
+dbName = 'sample_mflix',
+collection
 
 MongoClient.connect(dbConnectionStr)
-    .then(client => {
-        console.log('Connected to the database')
-        db = client.db(dbName)
-        collection = db.collection('movies')
-    })
-    .catch(err => console.log(err));
+.then(client => {
+    console.log('Connected to the database')
+    db = client.db(dbName)
+    collection = db.collection('movies')
+})
+.catch(err => console.log(err));
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'));
 
 app.get("/search", async (req, res) => {
     try {
